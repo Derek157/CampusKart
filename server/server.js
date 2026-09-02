116c34f5452ed860f4fcbb7e11264aecdba265c7
Nitin Thakur <nitinthakur@Nitins-MacBook-Air.local>
2026-09-03 01:49:20 +0530

    Allow production Vercel frontend

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(
     origin: [
       "http://localhost:5500",
       "http://127.0.0.1:5500",
-      "https://campus-kart-kappa.vercel.app"
+      "https://campus-kart-git-main-me-04ff.vercel.app/"
     ]
   })
 );
