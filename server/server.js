3012b7f5bca8b98e09a94f5ce9a9c1a8472df8af
Nitin Thakur <nitinthakur@Nitins-MacBook-Air.local>
2026-09-03 02:18:41 +0530

    Use open CORS for deployment

--- server/server.js
+++ server/server.js
@@ -25,15 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5500",
-      "http://127.0.0.1:5500",
-      "https://campus-kart-git-main-me-04ff.vercel.app"
-    ]
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
